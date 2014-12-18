614f84d34d27d8ebd4110619a0847ecf8e5b012b
Patrick Owen <patowen95@gmail.com>
2014-12-18 00:45:14 -0500

    Slight style changes and comments. Also included u and v into the permalink.

--- web-grapher/graph3d.js
+++ web-grapher/graph3d.js
@@ -1,30 +1,41 @@
 
+// Handles everything common between all 3D graphs. Some code is borrowed from the
+// WebGL tutorials which helped me get started.
+
+// Specifically: WebGL Lesson 7 – basic directional and ambient lighting | Learning WebGL
+// http://learningwebgl.com/blog/?p=684)
+
+
+
+// gl is the WebGL context, shaderProgram is the shader used for all
+// the drawing, and canvas is the given document element.
 var gl, shaderProgram, canvas;
 
-var mvMatrix = mat4.create();
-var mvMatrixStack = [];
-var pMatrix = mat4.create();
+var mvMatrix = mat4.create(); // Model-view matrix
+var pMatrix = mat4.create(); // Perspective matrix
 
-var vertexPositionBuffer;
-var vertexNormalBuffer;
-var vertexIndexBuffer;
+var vertexPositionBuffer; // Stores the vertex positions on the graph
+var vertexNormalBuffer; // Stores the normals of the graph
+var vertexIndexBuffer; // Stores the vertex traversal ordering in the graph
 
-var xRot = Math.PI/3;
-var zRot = 3*Math.PI/4;
-var distance = 4;
-var xCenter = 0, yCenter = 0, zCenter = 0;
+var xRot = Math.PI/3; // Vertical rotation in radians
+var zRot = 3*Math.PI/4; // Horizontal rotation in radians
+var distance = 4; // Distance from the center of the view
+var xCenter = 0, yCenter = 0, zCenter = 0; // Location of the center of the view
+var u = 0, v = 0;
 
-var mouseDown = false;
-var mouseX = 0, mouseY = 0;
-var mouseButton = 0;
+var mouseDown = false; // Whether the mouse button is pressed
+var mouseX = 0, mouseY = 0; // Location of the mouse with respect to the top-left of the canvas
+var mouseButton = 0; // Which mouse button is pressed
 
-var mouseSensitivityAngle = 0.01;
-var mouseSensitivityDistance = 1.003;
-var scrollSensitivityDistance = 1.0015;
+var mouseSensitivityAngle = 0.01; // How fast left-click dragging rotates the view
+var mouseSensitivityDistance = 1.003; // How fast right-click dragging zooms in and out
+var scrollSensitivityDistance = 1.0015; // How fast scrolling zooms in and out
 
-var showGraph = false;
-var showAxes = true;
+var showGraph = false; // Whether the graph is ready to be shown
+var showAxes = true; // Whether the axes are being shown
 
+// Code of the vertex shader
 var vertexShaderSrc = 
 	 "	attribute vec3 aVertexPosition;"
 	+"	attribute vec3 aVertexNormal;"
@@ -64,6 +75,7 @@ var vertexShaderSrc =
 	+"		}"
 	+"	}";
 
+// Code of the fragment shader
 var fragmentShaderSrc =
 	 "	precision mediump float;"
 	
@@ -78,22 +90,23 @@ var fragmentShaderSrc =
 	+"			gl_FragColor = vec4(vLightWeighting2, 1);"
 	+"	}";
 
+// Intialize WebGL
 function initGL()
 {
 	try
 	{
-		gl = canvas.getContext("experimental-webgl");
+		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
 		gl.viewportWidth = canvas.width;
 		gl.viewportHeight = canvas.height;
 	}
 	catch (e) {}
 	if (!gl)
 	{
-		alert("Could not initialise WebGL, sorry :-(");
+		alert("Could not initialise WebGL. Your browser may not support it.");
 	}
 }
 
-
+// Compiles the vertex shader and returns a reference to it.
 function getVertexShader(gl)
 {
 	var shader = gl.createShader(gl.VERTEX_SHADER);
@@ -109,6 +122,7 @@ function getVertexShader(gl)
 	return shader;
 }
 
+// Compiles the fragment shader and returns a reference to it.
 function getFragmentShader(gl)
 {
 	var shader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -124,7 +138,7 @@ function getFragmentShader(gl)
 	return shader;
 }
 
-
+// Initializes the shader program and attaches references to its uniforms to it.
 function initShaders()
 {
 	var fragmentShader = getFragmentShader(gl);
@@ -151,42 +165,27 @@ function initShaders()
 	shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
 	shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
 	shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, "uNMatrix");
-	shaderProgram.samplerUniform = gl.getUniformLocation(shaderProgram, "uSampler");
 	shaderProgram.useLightingUniform = gl.getUniformLocation(shaderProgram, "uUseLighting");
 	shaderProgram.ambientColorUniform = gl.getUniformLocation(shaderProgram, "uAmbientColor");
 	shaderProgram.lightingDirectionUniform = gl.getUniformLocation(shaderProgram, "uLightingDirection");
 	shaderProgram.directionalColorUniform = gl.getUniformLocation(shaderProgram, "uDirectionalColor");
 }
 
-function mvPushMatrix()
-{
-	var copy = mat4.create();
-	mat4.set(mvMatrix, copy);
-	mvMatrixStack.push(copy);
-}
-
-function mvPopMatrix()
-{
-	if (mvMatrixStack.length == 0)
-	{
-		throw "Invalid popMatrix!";
-	}
-	mvMatrix = mvMatrixStack.pop();
-}
-
-
+// Set the matrix uniforms to the current values of the matrices.
 function setMatrixUniforms()
 {
+	//Set the perspective and modelview matrices
 	gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
 	gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
-
+	
+	//Set normal matrix uniform properly (see http://learningwebgl.com/blog/?p=684 for details)
 	var normalMatrix = mat3.create();
 	mat4.toInverseMat3(mvMatrix, normalMatrix);
 	mat3.transpose(normalMatrix);
 	gl.uniformMatrix3fv(shaderProgram.nMatrixUniform, false, normalMatrix);
 }
 
-
+// Updates the graph if it is visible.
 function updateGraph()
 {
 	if (showGraph)
@@ -195,8 +194,12 @@ function updateGraph()
 	}
 }
 
+// Renders the graph and axes if necessary and updates the permalink to represent
+// what is currently being viewed.
 function render()
 {
+	document.getElementById("permalink").href = getQueryFromOptions();
+	
 	gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
@@ -222,13 +225,16 @@ function render()
 	}
 }
 
+// Renders the axes: Red=x, Green=y, Blue=z
 function renderAxes()
 {
+	// Make sure the axes are long enough
 	var axis_length = Math.abs(xCenter);
 	if (Math.abs(yCenter) > axis_length) axis_length = Math.abs(yCenter);
 	if (Math.abs(zCenter) > axis_length) axis_length = Math.abs(zCenter);
 	axis_length += distance*50;
 	
+	// Coordinates for the axes
 	var vertices =
 		[0,           0,           0,
 		 axis_length, 0,           0,
@@ -239,31 +245,31 @@ function renderAxes()
 	
 	var order = [0, 1, 0, 2, 0, 3];
 	
+	// Vertex positions
 	var axesVertexPositionBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, axesVertexPositionBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 	axesVertexPositionBuffer.itemSize = 3;
 	axesVertexPositionBuffer.numItems = vertices.length;
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, axesVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 	
+	// Vertex normals (all set to 0 because lighting is not used)
 	var axesVertexNormalBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, axesVertexNormalBuffer);
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STATIC_DRAW);
 	axesVertexNormalBuffer.itemSize = 3;
 	axesVertexNormalBuffer.numItems = normals.length;
-
+	gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, axesVertexNormalBuffer.itemSize, gl.FLOAT, false, 0, 0);
+	
+	// Vertex indices
 	var axesVertexIndexBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, axesVertexIndexBuffer);
 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(order), gl.STATIC_DRAW);
 	
-	//###########################################################
-	gl.bindBuffer(gl.ARRAY_BUFFER, axesVertexPositionBuffer);
-	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, axesVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, axesVertexNormalBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	
-	gl.uniform1i(shaderProgram.samplerUniform, 0);
+	// Turn lighting off
 	gl.uniform1i(shaderProgram.useLightingUniform, false);
 	
-	//gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, axesVertexIndexBuffer);
+	// Draw the axes
 	setMatrixUniforms();
 	gl.uniform3fv(shaderProgram.ambientColorUniform, [1, 0, 0]);
 	gl.drawElements(gl.LINES, 2, gl.UNSIGNED_SHORT, 0);
@@ -273,6 +279,7 @@ function renderAxes()
 	gl.drawElements(gl.LINES, 2, gl.UNSIGNED_SHORT, 8);
 }
 
+// Renders the graph based on the contents of the vertex, normal, and element buffers.
 function renderGraph()
 {
 	gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
@@ -280,8 +287,7 @@ function renderGraph()
 
 	gl.bindBuffer(gl.ARRAY_BUFFER, vertexNormalBuffer);
 	gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, vertexNormalBuffer.itemSize, gl.FLOAT, false, 0, 0);
-
-	gl.uniform1i(shaderProgram.samplerUniform, 0);
+	
 	gl.uniform1i(shaderProgram.useLightingUniform, true);
 	
 	gl.uniform3fv(shaderProgram.ambientColorUniform, [0.05, 0.05, 0.05]);
@@ -293,6 +299,7 @@ function renderGraph()
 	gl.drawElements(gl.TRIANGLES, vertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
 }
 
+// Sets all the buffers used in rendering the graph based on the given arrays.
 function setBuffers(vertices, normals, order)
 {
 	vertexPositionBuffer = gl.createBuffer();
@@ -314,7 +321,7 @@ function setBuffers(vertices, normals, order)
 	vertexIndexBuffer.numItems = order.length;
 }
 
-
+// Initializes WebGL and sets everything up.
 function webGLStart()
 {
 	canvas = document.getElementById("canvas");
@@ -323,10 +330,13 @@ function webGLStart()
 	initSettings();
 	
 	canvas.onmousedown = handleMouseDown;
+	
+	// Add mouse wheel events for all browsers
 	if (canvas.onmousewheel === undefined)
 		canvas.onwheel = handleWheel;
 	else
 		canvas.onmousewheel = handleMouseWheel;
+	
 	canvas.oncontextmenu = suppressContextMenu;
 	document.onmouseup = handleMouseUp;
 	document.onmousemove = handleMouseMove;
@@ -341,12 +351,14 @@ function webGLStart()
 	handleWindowResize();
 }
 
+// Prevents right-click dragging from opening a context menu.
 function suppressContextMenu(e)
 {
 	e.preventDefault();
 	return false;
 }
 
+// Makes axes toggle when the axes checkbox is toggled.
 function handleAxesUpdate(e)
 {
 	if (document.getElementById("axes").checked)
@@ -360,6 +372,7 @@ function handleAxesUpdate(e)
 	render();
 }
 
+// Makes hitting enter equivalent to pressing Apply.
 function handleEnterKey(e)
 {
 	if (e.keyCode == 13)
@@ -368,6 +381,7 @@ function handleEnterKey(e)
 	}
 }
 
+// Forces the canvas to take up the entire browser's space.
 function handleWindowResize(e)
 {
 	canvas.width = window.innerWidth;
@@ -379,6 +393,8 @@ function handleWindowResize(e)
 	render();
 }
 
+// Makes sure the program knows when the mouse is pressed and which button is pressed.
+// Also handles changing u and v when the mouse is pressed.
 function handleMouseDown(e)
 {
 	e.preventDefault();
@@ -393,14 +409,24 @@ function handleMouseDown(e)
 	if (e.ctrlKey)
 		mouseButton = 1;
 	
+	if (mouseButton == 1)
+	{
+		u = 2*e.clientX/canvas.width - 1;
+		v = 1 - 2*e.clientY/canvas.height;
+		updateGraph();
+		render();
+	}
+	
 	return false;
 }
 
+// Makes sure the program knows when the mouse is released.
 function handleMouseUp(e)
 {
 	mouseDown = false;
 }
 
+// Handles rotating, zooming, and changing u and v when the mouse is clicking and dragging.
 function handleMouseMove(e)
 {
 	if (mouseDown)
@@ -434,6 +460,7 @@ function handleMouseMove(e)
 	}
 }
 
+// Handles zooming while scrolling
 function handleMouseWheel(e)
 {
 	e.preventDefault();
@@ -442,6 +469,7 @@ function handleMouseWheel(e)
 	return false;
 }
 
+// Handles zooming while scolling (for Firefox)
 function handleWheel(e)
 {
 	e.preventDefault();
@@ -450,18 +478,23 @@ function handleWheel(e)
 	return false;
 }
 
+// Hides the settings menu
 function hideSettings()
 {
 	document.getElementById("settings").style.display = "none";
 	document.getElementById("show-settings").style.display = "inline-block";
 }
 
+// Shows the settings menu
 function showSettings()
 {
 	document.getElementById("settings").style.display = "inline-block";
 	document.getElementById("show-settings").style.display = "none";
 }
 
+// Returns the expression in the textfield with the given id and variable
+// list, or oldExpr if the expression is invalid. Also, this function turns
+// the textfield red if the expression is invalid and white if it is valid.
 function getExpression(id, vars, oldExpr)
 {
 	var element = document.getElementById(id);
@@ -485,6 +518,8 @@ function getExpression(id, vars, oldExpr)
 	}
 }
 
+// Turns an empty textfiled white. This makes sure that no red
+// textfields appear on a just-loaded page.
 function resetTextField(id)
 {
 	var element = document.getElementById(id);
@@ -494,6 +529,9 @@ function resetTextField(id)
 	}
 }
 
+// Returns the value in the textfield with the given id, or
+// oldExpr if the expression is invalid. Also, this function turns the
+// textfield red if the expression is invalid and white if it is valid.
 function getValue(id, oldValue)
 {
 	var element = document.getElementById(id);
@@ -519,6 +557,7 @@ function getValue(id, oldValue)
 	}
 }
 
+// Returns a string containing the query to put in the URL to match what is currently being displayed.
 function getQueryFromOptions()
 {
 	var query = "?";
@@ -534,10 +573,13 @@ function getQueryFromOptions()
 	query += "&zrot=" + zRot;
 	query += "&distance=" + distance;
 	query += "&showaxes=" + showAxes;
+	query += "&u=" + u;
+	query += "&v=" + v;
 	
 	return query;
 }
 
+// Matches all the parameters of the graph display to match everything in the query put in the URL.
 function setOptionsFromQuery()
 {
 	var query = window.location.search;
@@ -561,7 +603,7 @@ function setOptionsFromQuery()
 	}
 }
 
-// Sets a single text field based on the given encoded query.
+// Sets a single option based on the given encoded query.
 function setSingleOptionFromQuery(query)
 {
 	var divider = query.indexOf('=')
@@ -578,12 +620,16 @@ function setSingleOptionFromQuery(query)
 	}
 }
 
+// Sets an option based on the given query id and value if it is one
+// of the options compatible with all 3D graph types.
 function setCommonOption(id, value)
 {
 	if (id == "xrot") {xRot = parseFloatOption(value, xRot); return true;}
 	if (id == "zrot") {zRot = parseFloatOption(value, zRot); return true;}
 	if (id == "distance") {distance = parseFloatOption(value, distance); return true;}
 	if (id == "showaxes") {showAxes = parseBoolOption(value, showAxes); document.getElementById("axes").checked = showAxes; return true;}
+	if (id == "u") {u = parseFloatOption(value, u); return true;}
+	if (id == "v") {v = parseFloatOption(value, v); return true;}
 	return false;
 }
 
@@ -597,10 +643,10 @@ function parseFloatOption(str, oldValue)
 	return oldValue;
 }
 
-// Parses a float and returns oldValue if it cannot parse.
+// Parses a boolean and returns oldValue if it cannot parse.
 function parseBoolOption(str, oldValue)
 {
-	if (str === "true") return true;
-	if (str === "false") return false;
+	if (str == "true") return true;
+	if (str == "false") return false;
 	return oldValue;
 }
